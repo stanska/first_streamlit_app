b622e398b307e39c1c30f4ae6094794f62e01fca
Stanka Dalekova <stanska@gmail.com>
2023-04-24 14:12:29 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import streamlit;
 streamlit.title('My Parents New Healthy Dinner');
 streamlit.header('Breakfast menu');
 streamlit.text('Omega 3 & Oatmeal');
-streamlit.header('Spinachhi');
+streamlit.text('Spinachhi');
 
 
